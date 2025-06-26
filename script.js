89b9e64aa1b40da8df6b9926f4acd11794a113af
Madeleine Josephine Neaves <madeleineneaves@hotmail.com>
2025-06-26 17:53:40 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = "pasta"
+let rightGuessString = "emoji"
 
 console.log(rightGuessString)
 

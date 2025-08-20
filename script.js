678b483957d8f3d0b706f3cd9c06fe00a2a15948
Madeleine Josephine Neaves <madeleineneaves@hotmail.com>
2025-08-20 18:00:01 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = "slack"
+let rightGuessString = "origin"
 
 console.log(rightGuessString)
 

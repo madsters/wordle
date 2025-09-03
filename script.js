941689cf1a049b741a7a14bd407b1e37c5252e2e
Madeleine Josephine Neaves <madeleineneaves@hotmail.com>
2025-09-03 18:02:30 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = "women"
+let rightGuessString = "power"
 
 console.log(rightGuessString)
 

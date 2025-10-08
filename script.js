5c767fce5a7b3aa24a4e28b2a6c1b59fc4df9bfe
Madeleine Josephine Neaves <madeleineneaves@hotmail.com>
2025-10-08 18:08:22 +1100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = "chomp"
+let rightGuessString = "elect"
 
 console.log(rightGuessString)
 
